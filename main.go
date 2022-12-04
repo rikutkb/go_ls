e115b122d0e409e7041b6f196574564a9a858212
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2022-12-04 17:47:43 +0900

    feat: 時刻順でのソート機能を追加

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -19,23 +20,46 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	all := flag.Bool("a", false, "隠しファイルの表示を行います。")
+	var (
+		allFlag        bool
+		allmostAllFlag bool
+		sortTime       bool
+		sortReverse    bool
+		fullTime       bool
+	)
+	flag.BoolVar(&allFlag, "a", false, "隠しファイルの表示を行います。")
+	flag.BoolVar(&allFlag, "all", false, "隠しファイルの表示を行います。")
+	flag.BoolVar(&allmostAllFlag, "A", false, ".,..を除いた隠しファイルの表示を行います。")
+	flag.BoolVar(&allmostAllFlag, "allmost-all", false, ".,..を除いた隠しファイルの表示を行います。")
+	flag.BoolVar(&sortTime, "t", false, "新しい順に時刻でのソートを行います。")
+	flag.BoolVar(&sortReverse, "t", false, "並び順を逆にします。。")
+	flag.BoolVar(&fullTime, "full-time", false, "作成時刻の表示を行います。")
 
 	flag.Parse()
 
-	if *all {
-		for _, fileInfo := range fileInfos {
-			fmt.Println(fileInfo.Name())
-		}
-	} else {
-		for _, fileInfo := range fileInfos {
+	if !allFlag {
+		for i, fileInfo := range fileInfos {
 			if strings.HasPrefix(fileInfo.Name(), ".") {
-				continue
-			} else {
-				fmt.Println(fileInfo.Name())
+				fileInfos = fileInfos[:i+copy(fileInfos[i:], fileInfos[i+1:])]
 
 			}
 		}
+	}
+
+	// sort
+	if sortTime {
+		sort.SliceStable(fileInfos, func(i, j int) bool {
+			return fileInfos[i].ModTime().After(fileInfos[j].ModTime())
+		})
+	}
+	// if sortReverse {
+	// }
+	for _, fileInfo := range fileInfos {
+		fmt.Print(fileInfo.Name())
+		if fullTime {
+			fmt.Print(" " + fileInfo.ModTime().String())
+		}
+		fmt.Println()
 	}
 
 }
